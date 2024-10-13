13b50bc6ab562a50ac29767c324c4e33629972b2
nhutam03 <nhutam050@gmail.com>
2024-10-13 15:33:16 +0700

    [ADD] Thêm entity account và user

--- src/main/java/fit/iuh/modish_motion/entities/Account.java
+++ src/main/java/fit/iuh/modish_motion/entities/Account.java
@@ -1,4 +1,14 @@
 package fit.iuh.modish_motion.entities;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Account {
+    private int user_id;
+    private String username;
+    private String password;
+    private boolean isAdmin;
 }
--- src/main/java/fit/iuh/modish_motion/entities/User.java
+++ src/main/java/fit/iuh/modish_motion/entities/User.java
@@ -1,4 +1,19 @@
 package fit.iuh.modish_motion.entities;
 
+import java.util.Date;
+import lombok.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class User {
+    private int id;
+    private String name;
+    private String phone_number;
+    private String email;
+    private boolean gender;
+    private String address;
+    private Date dob;
 }
