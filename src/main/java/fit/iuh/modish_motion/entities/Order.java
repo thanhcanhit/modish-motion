76c5315b79e09c3abf4c81ff2f2ae3ce4a01cc3c
caubebutchii <caubebutchi2k3@gmail.com>
2024-10-13 15:41:19 +0700

    update entity
    update entities order, order_detail and pom

--- src/main/java/fit/iuh/modish_motion/entities/Order.java
+++ src/main/java/fit/iuh/modish_motion/entities/Order.java
@@ -1,4 +1,25 @@
 package fit.iuh.modish_motion.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@
 public class Order {
+    @Column
+    private int id;
+    private int status;
+    private Date order_at;
+    private Date cancelled_at;
+    private double total_due;
+    private int customer_id;
+    private boolean payment_confirm;
 }
