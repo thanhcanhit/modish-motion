da4fbe34448f4c75bd2f5e125e152b546c580056
kientrank3 <bankienthanthien@gmail.com>
2024-10-13 15:48:28 +0700

    Thêm entity Item và Variant

--- src/main/java/fit/iuh/modish_motion/entities/Item.java
+++ src/main/java/fit/iuh/modish_motion/entities/Item.java
@@ -1,4 +1,110 @@
 package fit.iuh.modish_motion.entities;
 
+
 public class Item {
+
+    private String id;
+    private String name;
+    private String characteristic;
+    private double promotion_price;
+    private String tags;
+    private int category_id;
+    private String gender;
+    private int quantity_sold;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    public Item() {
+    }
+
+    public Item(String id, String name, String characteristic, double promotion_price, String tags, int category_id, String gender, int quantity_sold) {
+        this.id = id;
+        this.name = name;
+        this.characteristic = characteristic;
+        this.promotion_price = promotion_price;
+        this.tags = tags;
+        this.category_id = category_id;
+        this.gender = gender;
+        this.quantity_sold = quantity_sold;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCharacteristic() {
+        return characteristic;
+    }
+
+    public void setCharacteristic(String characteristic) {
+        this.characteristic = characteristic;
+    }
+
+    public double getPromotion_price() {
+        return promotion_price;
+    }
+
+    public void setPromotion_price(double promotion_price) {
+        this.promotion_price = promotion_price;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getQuantity_sold() {
+        return quantity_sold;
+    }
+
+    public void setQuantity_sold(int quantity_sold) {
+        this.quantity_sold = quantity_sold;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", characteristic='" + characteristic + '\'' +
+                ", promotion_price=" + promotion_price +
+                ", tags='" + tags + '\'' +
+                ", category_id=" + category_id +
+                ", gender='" + gender + '\'' +
+                ", quantity_sold=" + quantity_sold +
+                '}';
+    }
 }
--- src/main/java/fit/iuh/modish_motion/entities/Variant.java
+++ src/main/java/fit/iuh/modish_motion/entities/Variant.java
@@ -1,4 +1,98 @@
 package fit.iuh.modish_motion.entities;
 
 public class Variant {
+
+    private String id;
+    private String name;
+    private String imageUrl;
+    private double price;
+    private int color_id;
+    private int product_id;;
+    private int availble_quantity;
+
+    public Variant(String id, String name, String imageUrl, double price, int color_id, int product_id, int availble_quantity) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.color_id = color_id;
+        this.product_id = product_id;
+        this.availble_quantity = availble_quantity;
+    }
+
+    public Variant() {
+    }
+
+    public Variant(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Variant{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", price=" + price +
+                ", color_id=" + color_id +
+                ", product_id=" + product_id +
+                ", availble_quantity=" + availble_quantity +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getColor_id() {
+        return color_id;
+    }
+
+    public void setColor_id(int color_id) {
+        this.color_id = color_id;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public int getAvailble_quantity() {
+        return availble_quantity;
+    }
+
+    public void setAvailble_quantity(int availble_quantity) {
+        this.availble_quantity = availble_quantity;
+    }
 }
