140f21b144eed4e53e2196d089b7da4f23ffe097
kientrank3 <bankienthanthien@gmail.com>
2024-11-15 14:48:02 +0700

    Update Slider

--- src/main/java/fit/iuh/modish_motion/controllers/HomeControllerDefault.java
+++ src/main/java/fit/iuh/modish_motion/controllers/HomeControllerDefault.java
@@ -33,14 +33,6 @@ public class HomeControllerDefault {
 
     @GetMapping("/homeDefault")
     public String homePage(Model model){
-        // Danh sách các ảnh banner
-        List<Map<String, String>> banners = List.of(
-                Map.of("imageUrl", "/images/home/imageBanner1.png", "link", ""),
-                Map.of("imageUrl", "/images/home/imageBanner2.png", "link", ""),
-                Map.of("imageUrl", "/images/home/imageBanner3.png", "link", ""),
-                Map.of("imageUrl", "/images/home/imageBanner4.jpg", "link", "")
-        );
-        model.addAttribute("banners", banners);
 
         return "homeDefault";
     }
