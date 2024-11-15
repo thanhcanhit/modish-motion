fc16e45d84132052713fa1e2a632c9c84ff8de5a
thanhcanhit <thanhcanh.dev@gmail.com>
2024-11-15 14:50:32 +0700

    Lấy lại việc nhắc lệnh, gắn favicon

--- src/main/java/fit/iuh/modish_motion/configs/WebConfig.java
+++ src/main/java/fit/iuh/modish_motion/configs/WebConfig.java
@@ -1,4 +1,15 @@
 package fit.iuh.modish_motion.configs;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
 }
